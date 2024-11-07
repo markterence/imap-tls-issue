78c1b859aae58f0aa5685be2299e491db2158ca6
Mark Terence <2003215+markterence@users.noreply.github.com>
2024-11-07 12:33:19 +0800

    feat: implement fetchEmails function and update index.js to retrieve inbox emails

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ import * as sogoImap from './src/sogoImap.js';
 const email = process.env.MAIL_USER;
 const password = process.env.MAIL_PASSWORD;
 console.log(process.env);
-await sogoImap.connectImap(email, password);
+const imap = await sogoImap.connectImap(email, password);
+const inbox = await sogoImap.fetchEmails('INBOX', imap);
+console.log(inbox);
--- src/sogoImap.js
+++ src/sogoImap.js
@@ -23,3 +23,73 @@ export const connectImap = async (email, password) => {
     throw new Error('Error connecting to IMAP server');
   }
 };
+
+export const fetchEmails = async (folder = 'INBOX', imap, page = 1, limit = 10, searchQuery = []) => {
+  return new Promise((resolve, reject) => {
+    imap.openBox(folder.replace('folder', ''), true, (err, box) => {
+      if (err) return reject(err);
+
+      const emails = [];
+
+      const searchCriteria = searchQuery.length ? searchQuery : ['ALL'];
+
+      // Perform search based on the searchQuery
+      imap.search(searchCriteria, (err, results) => {
+        if (err) return reject(err);
+
+        const totalMessages = results.length;
+        const start = (page - 1) * limit;
+        const end = Math.min(start + limit, totalMessages);
+        const fetchRange = results.reverse().slice(start, end);
+
+        if (!fetchRange.length) {
+          return resolve({
+            emails,
+            currentPage: page,
+            totalMessages,
+            totalPages: Math.ceil(totalMessages / limit)
+          });
+        }
+
+        const f = imap.fetch(fetchRange, {
+          bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+          struct: true,
+          uid: true
+        });
+
+        f.on('message', (msg, seqno) => {
+          let mailObject = {};
+
+          msg.on('attributes', (attrs) => {
+            mailObject.uid = attrs.uid;
+            mailObject.seen = attrs.flags.includes('\\Seen');
+          });
+
+          msg.on('body', (stream) => {
+            let buffer = '';
+            stream.on('data', (chunk) => {
+              buffer += chunk.toString('utf8');
+            });
+            stream.once('end', () => {
+              mailObject.headers = Imap.parseHeader(buffer);
+              emails.push(mailObject);
+            });
+          });
+        });
+
+        f.once('error', (err) => {
+          reject(err);
+        });
+
+        f.once('end', () => {
+          resolve({
+            emails: emails.reverse(),
+            currentPage: page,
+            totalMessages,
+            totalPages: Math.ceil(totalMessages / limit)
+          });
+        });
+      });
+    });
+  });
+};
