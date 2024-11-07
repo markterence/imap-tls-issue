10a56bd45d6c4b8130360f70d742be077a966ad5
Mark Terence <2003215+markterence@users.noreply.github.com>
2024-11-07 12:36:03 +0800

    fix: remove DNS options from docker-compose.yml and log inbox emails in a formatted way

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const password = process.env.MAIL_PASSWORD;
 console.log(process.env);
 const imap = await sogoImap.connectImap(email, password);
 const inbox = await sogoImap.fetchEmails('INBOX', imap);
-console.log(inbox);
+console.log(JSON.stringify(inbox, null, 2));
