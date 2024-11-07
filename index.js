2db662d9e6f9b82a5ca1f28cdf3364005e31b879
Mark Terence <2003215+markterence@users.noreply.github.com>
2024-11-07 11:36:44 +0800

    fix: log environment variables for debugging IMAP connection

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import * as sogoImap from './src/sogoImap.js';
 
 const email = process.env.MAIL_USER;
 const password = process.env.MAIL_PASSWORD;
+console.log(process.env);
 await sogoImap.connectImap(email, password);
